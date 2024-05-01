7503f5039a1f64da01da9ec83bd54b375a440296
Muhammad YaSSiR <mhdyassir2018@gmail.com>
2024-05-01 07:08:17 +0700

    pertemuan 2 tanggal 30 april 2024 tugas pribadi

---
+++ PertemuanKe2Pribadi/TugasPapanTulis.js
@@ -0,0 +1,34 @@
+/*
+const readLineSync = require('readline-sync');
+
+        let a = parseFloat(readLineSync.question('masukkan nilai untuk a: '));
+        let b = parseFloat(readLineSync.question('masukkan nilai untuk b: '));
+        let c = parseFloat(readLineSync.question('masukkan nilai untuk c: '));
+
+        let hitung = a + b - c ;
+
+        console.log("Jumlah nilai a + b - c : " + hitung);
+        
+*/
+
+const readLineSync = require('readline-sync');
+
+        let t = parseFloat(readLineSync.question('masukkan nilai untuk t: '));
+        let y = parseFloat(readLineSync.question('masukkan nilai untuk y: '));
+
+        let hitung = t + y ** 2 ;
+
+        console.log("Jumlah nilai t + y ** 2 : " + hitung);
+
+/**
+const readLineSync = require('readline-sync');
+
+        let a = parseFloat(readLineSync.question('masukkan nilai untuk a: '));
+        let b = parseFloat(readLineSync.question('masukkan nilai untuk b: '));
+        let c = parseFloat(readLineSync.question('masukkan nilai untuk c: '));
+        let d = parseFloat(readLineSync.question('masukkan nilai untuk d: '));
+
+        let hitung = (a * b ** 2) / c + d ** 2;
+
+        console.log("Jumlah nilai (a * b ** 2) / c + d ** 2 : " + hitung);
+ */
