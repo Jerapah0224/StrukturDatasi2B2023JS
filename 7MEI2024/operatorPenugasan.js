fffea9c82fd53cdd8644c2fe80ce8f0be0343a39
Muhammad YaSSiR <mhdyassir2018@gmail.com>
2024-05-07 14:01:34 +0700

    Pertemuan ke 7

---
+++ 7MEI2024/operatorPenugasan.js
@@ -0,0 +1,6 @@
+let angka1 = parseFloat(prompt("Masukkan angka pertama: "));
+let angka2 = parseFloat(prompt("Masukkan angka kedua: "));
+
+let total = angka1 * angka2;
+
+console.log("Hasil perkalian: " + total);
