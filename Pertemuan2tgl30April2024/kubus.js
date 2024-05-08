2d05ae13931d8d0866441556945ab6ce8964c797
MuhammadYassir114 <130085304+Jerapah0224@users.noreply.github.com>
2024-05-08 13:19:45 +0700

    Delete Pertemuan2tgl30April2024 directory

--- Pertemuan2tgl30April2024/kubus.js
+++
@@ -1,7 +0,0 @@
-const readLineSync = require('readline-sync');
-
-let sisi = parseFloat(readLineSync.question('Masukkan sisi kubus: '));
-
-let luasPermukaan = 6 * (sisi * sisi);
-
-console.log("Luas Permukaan Kubus: " + luasPermukaan);
